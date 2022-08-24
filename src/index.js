99b07b161d346243014cd7abcc54b49b4fa9684c
Steven McVey <smcvey1210@gmail.com>
2022-08-23 22:05:35 -0400

    completed tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    handleTodo(e.target['new-task-description'].value);
+    form.reset();
+  })
 });
+
+const redTasks = document.querySelector('#redTasks')
+const orangeTasks = document.querySelector('#orangeTasks')
+const greenTasks = document.querySelector('#greenTasks')
+
+
+function handleTodo(todo){
+  let li = document.createElement('li');
+  let btn = document.createElement('button')
+  let urgentness = document.querySelector('#urgency').value;
+  btn.textContent = 'complete'
+  li.textContent = `${todo} `
+  li.className = `${urgentness}urgent`
+  li.appendChild(btn)
+  btn.addEventListener('click', handleDelete)
+  if (urgentness === '0'){
+    greenTasks.appendChild(li);
+    li.style.color = 'white'
+  }else if (urgentness === '1'){
+    orangeTasks.appendChild(li);
+    li.style.color = 'white'
+  }else{
+    redTasks.appendChild(li);
+    li.style.color = 'white'
+  }
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
+let range = document.getElementById('urgency');
+
